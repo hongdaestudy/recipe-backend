3ca95c79d2fc6d6b04fb39484670985a6c6315d6
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-09-13 22:02:47 +0900

    chore: 패키지 구조 확인을 위해 레시피컨트롤러 간단하게 구현

--- src/main/java/com/hongdaestudy/recipebackend/recipe/presentation/RecipeCommandController.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/presentation/RecipeCommandController.java
@@ -1,8 +1,22 @@
 package com.hongdaestudy.recipebackend.recipe.presentation;
 
-import org.springframework.stereotype.Controller;
+import com.hongdaestudy.recipebackend.recipe.application.RegisterRecipeService;
+import com.hongdaestudy.recipebackend.recipe.application.in.RecipeCommand;
+import com.hongdaestudy.recipebackend.recipe.application.out.RecipeCommandResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class RecipeCommandController {
 
+  private final RegisterRecipeService registerRecipeService;
+
+  @PostMapping("/recipes")
+  public ResponseEntity<RecipeCommandResult> registerRecipe(RecipeCommand command) {
+    RecipeCommandResult result = registerRecipeService.createRecipe(command);
+    return ResponseEntity.ok(result);
+  }
 }

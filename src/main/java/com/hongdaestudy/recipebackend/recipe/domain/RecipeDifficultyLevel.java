1157f8513f101e089bade0aaa583ff7cc24e4fe5
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-09-14 23:18:59 +0900

    chore: 논의 끝난 todo 제거

--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeDifficultyLevel.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeDifficultyLevel.java
@@ -3,8 +3,6 @@ package com.hongdaestudy.recipebackend.recipe.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-// TODO jaesay: 1. 지금, 2. 레시피 엔티티 이너 클래스로 할까.., 3. 아니면 domain 하위 패이지에서 관리?
-// TODO jaesay: 만개의 레시피 실제값이랑 다르게 그냥 쉬움, 보통, 어려움으로 해도 될지..
 @AllArgsConstructor
 @Getter
 public enum RecipeDifficultyLevel {
--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeServingCount.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeServingCount.java
@@ -3,7 +3,6 @@ package com.hongdaestudy.recipebackend.recipe.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-// TODO jaesay: 1. 지금, 2. 레시피 엔티티 이너 클래스로 할까.., 3. 아니면 domain 하위 패이지에서 관리?
 @AllArgsConstructor
 @Getter
 public enum RecipeServingCount {

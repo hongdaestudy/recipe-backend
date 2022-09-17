68949e0df345339082c2f1d789922b36162f2b68
Kim Jae Seong <jaesay@ajou.ac.kr>
2022-09-17 22:17:11 +0900

    fix: RecipeRepository ID 수정

--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/repository/RecipeRepository.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/repository/RecipeRepository.java
@@ -1,7 +1,8 @@
 package com.hongdaestudy.recipebackend.recipe.domain.repository;
 
 import com.hongdaestudy.recipebackend.recipe.domain.Recipe;
+import com.hongdaestudy.recipebackend.recipe.domain.RecipeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, RecipeId> {
 }

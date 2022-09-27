ed4a96f28e5ef091928f1f70ce5442f37f95b5d8
suwampy <tndhks123@nate.com>
2022-09-28 02:13:59 +0900

    BE-7 feat: user entity 구현

---
+++ src/main/java/com/hongdaestudy/recipebackend/user/domain/User.java
@@ -0,0 +1,37 @@
+package com.hongdaestudy.recipebackend.user.domain;
+
+import com.hongdaestudy.recipebackend.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity<User, Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_id")
+    private UserProfile userProfile;
+
+    private String email;
+
+    private String password;
+
+    // todo: validate
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void changeUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+}

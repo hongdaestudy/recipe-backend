03f16b36e57ea968bc02aea5c265d57e5c800ed0
suwampy <tndhks123@nate.com>
2022-09-30 00:41:49 +0900

    chore: 커밋 안된 부분 수정

--- src/main/java/com/hongdaestudy/recipebackend/recipe/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hongdaestudy.recipebackend.recipe.repository;
-
-import com.hongdaestudy.recipebackend.recipe.domain.RecipeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
-}

fbfa2269f88867ffd11b7b7c46a0fc0cbd2fad7a
zion <lce322@naver.com>
2022-10-03 13:52:08 +0900

    feat: Equals() HashCode() 추가
    Signed-off-by: zion <lce322@naver.com>

--- src/main/java/com/hongdaestudy/recipebackend/comment/domain/Comment.java
+++ src/main/java/com/hongdaestudy/recipebackend/comment/domain/Comment.java
@@ -6,8 +6,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Table(name = "comment")
 @Entity
@@ -61,5 +63,18 @@ public class Comment extends BaseTimeEntity {
     comment.photoFileId = photoFileId;
 
     return comment;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    Comment comment = (Comment) o;
+    return id != null && Objects.equals(id, comment.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
   }
 }

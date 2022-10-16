89a410831680b9bf8e0b4074489c8ad6ef958fc6
zion <lce322@naver.com>
2022-10-17 01:00:44 +0900

    test: 댓글 조회 Repository 테스트 코드
    Signed-off-by: zion <lce322@naver.com>

--- src/test/java/com/hongdaestudy/recipebackend/comment/application/RegisterCommentServiceTest.java
+++ src/test/java/com/hongdaestudy/recipebackend/comment/application/RegisterCommentServiceTest.java
@@ -38,13 +38,38 @@ public class RegisterCommentServiceTest {
 
       @Test
       @DisplayName("주어진 객체를 저장하고, 저장된 객체를 리턴한다")
-      void it_saves_obj_and_returns_a_saved_obj() {
+      void it_saves_obj_and_returns_a_saved_obj() throws Exception {
 
         RegisterCommentCommandResult result = registerCommentService.registerComment(command);
         Assertions.assertNotNull(result.getId(), "저장된 객체는 아이디가 추가되어 있다");
 
         Optional<Comment> saved = repository.findById(result.getId());
         Assertions.assertEquals(saved.get().getContent(), givenContent);
+      }
+    }
+
+    @Nested
+    @DisplayName("등록 객체가 주어지면")
+    class Context_with_comments {
+      RegisterCommentCommand parent = new RegisterCommentCommand(1L, 1L, givenContent, null, 0, 1, "4.5", null);
+      RegisterCommentCommand child1 = new RegisterCommentCommand(1L, 2L, givenContent, 1L, 1, 1, "4.0", null);
+      RegisterCommentCommand child2 = new RegisterCommentCommand(1L, 3L, givenContent, 1L, 1, 2, "3.0", null);
+      RegisterCommentCommand child3 = new RegisterCommentCommand(1L, 4L, givenContent, 2L, 2, 1, "2.0", null);
+
+      @Test
+      @DisplayName("주어진 객체를 저장하고, 저장된 객체를 리턴한다")
+      void it_saves_obj_and_returns_a_saved_obj() throws Exception {
+
+        RegisterCommentCommandResult result1 = registerCommentService.registerComment(parent);
+        Assertions.assertNotNull(result1.getId(), "저장된 객체는 아이디가 추가되어 있다");
+
+        RegisterCommentCommandResult result2 = registerCommentService.registerComment(child1);
+        Assertions.assertNotNull(result2.getId(), "저장된 객체는 아이디가 추가되어 있다");
+        RegisterCommentCommandResult result3 = registerCommentService.registerComment(child2);
+        Assertions.assertNotNull(result3.getId(), "저장된 객체는 아이디가 추가되어 있다");
+        RegisterCommentCommandResult result4 = registerCommentService.registerComment(child3);
+        Assertions.assertNotNull(result4.getId(), "저장된 객체는 아이디가 추가되어 있다");
+
       }
     }
   }

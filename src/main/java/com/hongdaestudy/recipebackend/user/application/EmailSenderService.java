cb62cd5e3577ad5896f7da8f9569ce73bdabb4cd
suwampy <tndhks123@nate.com>
2022-10-26 17:35:01 +0900

    BE-43 feat: 이메일 인증 메일 전송 API 구현

---
+++ src/main/java/com/hongdaestudy/recipebackend/user/application/EmailSenderService.java
@@ -0,0 +1,56 @@
+package com.hongdaestudy.recipebackend.user.application;
+
+import com.hongdaestudy.recipebackend.user.application.in.EmailCodeCommand;
+import com.hongdaestudy.recipebackend.user.application.in.EmailCommand;
+import com.hongdaestudy.recipebackend.user.application.out.EmailCodeResult;
+import com.hongdaestudy.recipebackend.util.RedisUtils;
+import io.netty.util.internal.StringUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class EmailSenderService {
+    private final JavaMailSender emailSender;
+    private final RedisUtils redisUtil;
+
+    public EmailCodeResult sendEmailMessage(EmailCommand email) {
+        String code = createCode();
+        redisUtil.setDataExpire(code, email.getEmail(),  60 * 30L);
+
+        log.debug("이메일 전송 중...");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email.getEmail());
+        message.setSubject("회원가입 인증 메일입니다. ");
+        message.setText(code);
+        emailSender.send(message);
+        log.debug("이메일 전송 완료");
+
+        return EmailCodeResult.from(code);
+    }
+    private static String createCode() {
+        StringBuilder code = new StringBuilder();
+        Random rnd = new Random();
+        for (int i = 0; i < 7; i++) {
+            int rIndex = rnd.nextInt(3);
+            switch (rIndex) {
+                case 0:
+                    code.append((char) (rnd.nextInt(26) + 97));
+                    break;
+                case 1:
+                    code.append((char) (rnd.nextInt(26) + 65));
+                    break;
+                case 2:
+                    code.append((rnd.nextInt(10)));
+                    break;
+            }
+        }
+        return code.toString();
+    }
+}

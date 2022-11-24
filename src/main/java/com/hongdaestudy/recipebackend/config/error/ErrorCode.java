fcc671b4f81167eee460285a109bd09844aedc67
suwampy <tndhks123@nate.com>
2022-11-25 00:40:21 +0900

    chore: error code 오탈자 수정

--- src/main/java/com/hongdaestudy/recipebackend/config/error/ErrorCode.java
+++ src/main/java/com/hongdaestudy/recipebackend/config/error/ErrorCode.java
@@ -4,8 +4,6 @@ public enum ErrorCode {
   HANDLE_ACCESS_DENIED(403, "C_001", "권한이 없습니다."),
   MEMBER_NOT_FOUND(400, "C_002", "사용자 정보를 찾을 수 없습니다."),
   ACCESS_TOKEN_EXPIRED(400, "C_003", "토큰이 만료되었습니다."),
-  HANDLE_ACCESS_DENIED(403, "C_001", "권한이 없습니다."),
-  MEMBER_NOT_FOUND(400, "C_002", "사용자 정보를 찾을 수 없습니다."),
 
   SCRAP_DUPLICATION(409, "C_003", "이미 스크랩된 레시피 입니다."),
   COMMENT_NOT_FOUND(400, "C_004", "부모 댓글을 찾을 수 없습니다.");

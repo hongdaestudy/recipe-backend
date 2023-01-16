79f69fc94b6e24c47e92fa3f3761f0d2b865ace5
정선우 <laborlawseon@gmail.com>
2023-01-16 23:18:57 +0900

    .

--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/repository/RecipeRepository.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/repository/RecipeRepository.java
@@ -2,13 +2,6 @@ package com.hongdaestudy.recipebackend.recipe.domain.repository;
 
 import com.hongdaestudy.recipebackend.recipe.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long>, RecipeRepositoryCustom {
-
-    @Modifying
-    @Query(value = "update Recipe set delete_at = 'Y' where recipe_id = :recipeId", nativeQuery = true)
-    void deleteRecipe(@Param("recipeId") Long recipeId);
 }

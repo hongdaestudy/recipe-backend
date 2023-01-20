c4da4acdc77c79d937cbe90618d7e45babad97dc
Esjay <esjaythegreat@gmail.com>
2023-01-21 00:28:52 +0900

    CORS 설정 추가

--- src/main/java/com/hongdaestudy/recipebackend/RecipeBackendApplication.java
+++ src/main/java/com/hongdaestudy/recipebackend/RecipeBackendApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RecipeBackendApplication extends SpringBootServletInitializer {
@@ -15,5 +19,14 @@ public class RecipeBackendApplication extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(RecipeBackendApplication.class);
+  }
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://1000recipe.esjay.click");
+      }
+    };
   }
 }

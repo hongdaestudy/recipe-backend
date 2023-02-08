96a55cbf57cc5edbc2cd5361f3190c6e7452e99e
suwampy <tndhks123@nate.com>
2023-02-08 18:56:36 +0900

    fix: redis 설정, 메일 로직 제거
    - 배포 편의를 위해 기능 제거

--- src/main/java/com/hongdaestudy/recipebackend/user/presentation/EmailCommandController.java
+++ src/main/java/com/hongdaestudy/recipebackend/user/presentation/EmailCommandController.java
@@ -1,29 +1,29 @@
-package com.hongdaestudy.recipebackend.user.presentation;
-
-import com.hongdaestudy.recipebackend.user.application.EmailSenderService;
-import com.hongdaestudy.recipebackend.user.application.in.EmailCodeCommand;
-import com.hongdaestudy.recipebackend.user.application.in.EmailCommand;
-import com.hongdaestudy.recipebackend.user.application.out.EmailCodeResult;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/auth")
-public class EmailCommandController {
-    private final EmailSenderService emailSenderService;
-
-    @PostMapping("/email-send")
-    public ResponseEntity<EmailCodeResult> sendEmail(@RequestBody EmailCommand request) {
-        return ResponseEntity.ok(emailSenderService.sendEmailMessage(request));
-    }
-
-    @PostMapping("/verify-code")
-    public ResponseEntity<Boolean> verifyCode(@RequestBody EmailCodeCommand request) {
-        return ResponseEntity.ok(emailSenderService.verifyEmail(request));
-    }
-}
+//package com.hongdaestudy.recipebackend.user.presentation;
+//
+//import com.hongdaestudy.recipebackend.user.application.EmailSenderService;
+//import com.hongdaestudy.recipebackend.user.application.in.EmailCodeCommand;
+//import com.hongdaestudy.recipebackend.user.application.in.EmailCommand;
+//import com.hongdaestudy.recipebackend.user.application.out.EmailCodeResult;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/auth")
+//public class EmailCommandController {
+//    private final EmailSenderService emailSenderService;
+//
+//    @PostMapping("/email-send")
+//    public ResponseEntity<EmailCodeResult> sendEmail(@RequestBody EmailCommand request) {
+//        return ResponseEntity.ok(emailSenderService.sendEmailMessage(request));
+//    }
+//
+//    @PostMapping("/verify-code")
+//    public ResponseEntity<Boolean> verifyCode(@RequestBody EmailCodeCommand request) {
+//        return ResponseEntity.ok(emailSenderService.verifyEmail(request));
+//    }
+//}

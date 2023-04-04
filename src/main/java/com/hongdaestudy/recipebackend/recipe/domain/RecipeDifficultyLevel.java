93050bdb468e1922cf916996bcc7495a4a3cdda6
sieun0322 <lce322@naver.com>
2023-04-05 01:26:42 +0900

    feat: 레시피 등록 enum 조회 API
    Signed-off-by: sieun0322 <lce322@naver.com>

--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeDifficultyLevel.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeDifficultyLevel.java
@@ -1,14 +1,24 @@
 package com.hongdaestudy.recipebackend.recipe.domain;
 
+import com.hongdaestudy.recipebackend.common.EnumMapperType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum RecipeDifficultyLevel {
+public enum RecipeDifficultyLevel implements EnumMapperType {
   EASY("쉬움"),
   NORMAL("보통"),
   DIFFICULT("어려움");
 
   private final String description;
+  @Override
+  public String getCode() {
+    return name();
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
 }
--- src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeSituation.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/domain/RecipeSituation.java
@@ -1,11 +1,12 @@
 package com.hongdaestudy.recipebackend.recipe.domain;
 
+import com.hongdaestudy.recipebackend.common.EnumMapperType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum RecipeSituation {
+public enum RecipeSituation implements EnumMapperType {
   ROUTINE("일상"),
   SPEED("초스피드"),
   SERVE("손님접대"),
@@ -22,4 +23,14 @@ public enum RecipeSituation {
   OTHERS("기타");
 
   private final String description;
+
+  @Override
+  public String getCode() {
+    return name();
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
 }

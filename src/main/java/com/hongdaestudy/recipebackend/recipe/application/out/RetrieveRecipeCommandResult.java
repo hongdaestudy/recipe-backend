f5136b4851d2bc53515d5722efcde1c8ad57309f
seonwoo_jung <79202163+jungseonw00@users.noreply.github.com>
2023-05-02 19:41:24 +0900

    Update RetrieveRecipeCommandResult.java

--- src/main/java/com/hongdaestudy/recipebackend/recipe/application/out/RetrieveRecipeCommandResult.java
+++ src/main/java/com/hongdaestudy/recipebackend/recipe/application/out/RetrieveRecipeCommandResult.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toList;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
